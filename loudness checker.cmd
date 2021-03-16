df9ef9be653b076a4f59213fa3be7bdf10417f83
nxanywhere <79404127+nxanywhere@users.noreply.github.com>
2021-03-17 03:38:16 +0700

    Delete loudness checker.cmd

--- loudness checker.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-title loudness analyser
-echo -------------------------------------
-set /P "browse=input... (Drag here!):"
-echo -------------------------------------
-ffmpeg -nostats -i "%browse%" -filter_complex ebur128=peak=true -f null -
-cmd
