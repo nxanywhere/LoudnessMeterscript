85c81e3b696d0b0c106f46fa65d091dccb48adb5
nxanywhere <79404127+nxanywhere@users.noreply.github.com>
2021-03-19 01:16:20 +0700

    Update loudnesschecker.cmd

--- loudnesschecker.cmd
+++ loudnesschecker.cmd
@@ -1,7 +1,14 @@
 @echo off
-title loudness analyser
-echo -------------------------------------
-set /P "browse=input... (Drag here!):"
-echo -------------------------------------
-ffmpeg -nostats -i "%browse%" -filter_complex ebur128=peak=true -f null -
-cmd
+setlocal
+set "ps=Add-Type -AssemblyName System.windows.forms;"
+set "ps=%ps% $f = New-Object System.Windows.Forms.OpenFileDialog;"
+set "ps=%ps% $f.Filter = 'Audio Files (*.wav)|*.wav|All Files (*.*)|*.*';"
+set "ps=%ps% $f.showHelp = $true;"
+set "ps=%ps% $f.ShowDialog();"
+set "ps=%ps% $f.FileName"
+
+for /f "delims=" %%I in ('powershell "%ps%"') do set "Filename=%%I"
+
+ffmpeg -nostats -i "%Filename%" -filter_complex ebur128=peak=true -f null -
+pause
+goto :EOF
